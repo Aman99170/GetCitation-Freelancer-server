5c0e1bcaee20909480525683d279173832c033e2
Aman Singh <amansingh99170@gmail.com>
2025-04-04 14:39:51 +0530

    checking hibernate connection

--- src/main/java/com/Freelancer/getcitations_freelancer/service/BidWinnerService.java
+++ src/main/java/com/Freelancer/getcitations_freelancer/service/BidWinnerService.java
@@ -1,11 +1,5 @@
 package com.Freelancer.getcitations_freelancer.service;
 
-import java.math.BigDecimal;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,10 +9,6 @@ import com.Freelancer.getcitations_freelancer.model.UserModel;
 
 @Service
 public class BidWinnerService {
-	
-	static final String DB_URL = "jdbc:mysql://localhost:3306/getcitation";
-    static final String USER = "root";
-    static final String PASS = "Belapur5#";
 
 	@Autowired
 	private BidWinnerRepository bwRepo;
@@ -27,41 +17,5 @@ public class BidWinnerService {
 		return winner.getWinnerId();
 	}
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-//	public void test() {
-//		Connection conn = null;
-//		PreparedStatement pstmt = null;
-//		ResultSet rs = null;
-//		BigDecimal numcheck = new BigDecimal(0.00000000000);
-//		try {
-//		conn = DriverManager.getConnection(DB_URL, USER, PASS);
-//		String str = "select value from getcitation.test";
-//		pstmt = conn.prepareStatement(str);
-//        rs = pstmt.executeQuery();
-//        while(rs.next()) {
-//        	System.out.println("Outside"+rs.getObject(1).toString());
-//        	BigDecimal num = (BigDecimal)rs.getObject(1);
-//       	 if(!num.equals(numcheck)) {
-//       		 System.out.println("inside"+rs.getObject(1).toString());
-//       	 }
-//        }
-//		}catch(Exception e) {
-//			e.printStackTrace();
-//		}
-//		
-//	}
 
 }
--- src/main/java/com/Freelancer/getcitations_freelancer/util/HibernateUtil.java
+++ src/main/java/com/Freelancer/getcitations_freelancer/util/HibernateUtil.java
@@ -7,24 +7,59 @@ import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.cfg.Environment;
 import org.hibernate.service.ServiceRegistry;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 import com.Freelancer.getcitations_freelancer.model.BidWinnerModel;
 import com.Freelancer.getcitations_freelancer.model.ReseachPaperBiddingModel;
 import com.Freelancer.getcitations_freelancer.model.ReviewsModel;
 import com.Freelancer.getcitations_freelancer.model.UserModel;
 
+import jakarta.annotation.PostConstruct;
+
+//@Component
 public class HibernateUtil {
     private static SessionFactory sessionFactory;
+    
+//    @Value("${db.host}")
+//    private static String dbHost;
+//
+//    @Value("${db.port}")
+//    private static String dbPort;
+//
+//    @Value("${db.database}")
+//    private static String dbName;
+//
+//    @Value("${db.user}")
+//    private static String dbUsername;
+//
+//    @Value("${db.password}")
+//    private static String dbPassword;
+//    
+//    @PostConstruct
     public static SessionFactory getSessionFactory() {
+    	
+    	System.getenv().forEach((k, v) -> System.out.println("key: " + k + ", value: " + v));
+    	String dbHost = System.getenv("MYSQL_HOST") != null ? System.getenv("MYSQL_HOST") : "localhost";
+        String dbPort = System.getenv("MYSQL_PORT") != null ? System.getenv("MYSQL_PORT") : "3306";
+        String dbName = System.getenv("MYSQL_DB_NAME") != null ? System.getenv("MYSQL_DB_NAME") : "getcitation";
+        String dbUsername = System.getenv("MYSQL_USER") != null ? System.getenv("MYSQL_USER") : "root";
+        String dbPassword = System.getenv("MYSQL_PASSWORD") != null ? System.getenv("MYSQL_PASSWORD") : "Belapur5#";
+        
+        System.out.println("dbhost"+dbHost);
+        System.out.println("dbport"+dbPort);
+        System.out.println("dbname"+dbName);
+        System.out.println("username"+dbUsername);
+        System.out.println("dbpassword"+dbPassword);
         if (sessionFactory == null) {
             try {
                 Configuration configuration = new Configuration();
 
                 Properties settings = new Properties();
                 settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
-                settings.put(Environment.URL, "jdbc:mysql://localhost:3306/GetCitation?useSSL=false");
-                settings.put(Environment.USER, "root");
-                settings.put(Environment.PASS, "Belapur5#");
+                settings.put(Environment.URL, "jdbc:mysql://"+dbHost+":"+dbPort+"/"+dbName+"?useSSL=false");
+                settings.put(Environment.USER, dbUsername);
+                settings.put(Environment.PASS, dbPassword);
 
                 settings.put(Environment.SHOW_SQL, "true");
 
@@ -44,6 +79,7 @@ public class HibernateUtil {
 
                 sessionFactory = configuration.buildSessionFactory(serviceRegistry);
             } catch (Exception e) {
+            	System.out.println("error in creating sessionFactory"+e.getMessage());
                 e.printStackTrace();
             }
         }
